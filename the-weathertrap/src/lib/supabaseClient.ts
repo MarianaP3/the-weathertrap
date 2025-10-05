68e5a53da9b12623f92063808aaf3a8116b67b83
Paola Bustos <paopaopro1808@gmail.com>
2025-10-05 04:35:18 -0600

    Login has been implemented

--- the-weathertrap/src/lib/supabaseClient.ts
+++ the-weathertrap/src/lib/supabaseClient.ts
@@ -1,15 +1,7 @@
 /// <reference types="vite/client" />
-import { createClient, SupabaseClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl: string = import.meta.env.SUPABASE_URL as string
-const supabaseKey: string = import.meta.env.SUPABASE_KEY as string
+const supabaseUrl: string = import.meta.env.PUBLIC_SUPABASE_URL as string
+const supabaseKey: string = import.meta.env.PUBLIC_SUPABASE_ANON_KEY as string
 
-export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey)
-
-// Example type for a "tasks" table
-/* export type Task = {
-  id: number
-  title: string
-  completed: boolean
-} */
-
+export const supabase = createClient(supabaseUrl, supabaseKey)
